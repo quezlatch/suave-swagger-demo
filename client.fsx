590e2082806231dcae7fa6b79278d2de07251e79
Michael Dickens <michaeldickens@code591.corp.codecomputerlove.com>
2017-03-17 15:49:32 +0000

    update client

--- client.fsx
+++ client.fsx
@@ -2,5 +2,8 @@
 open SwaggerProvider
 let [<Literal>] Schema = "http://localhost:8080/swagger/v2/swagger.json"
 type OperandClient = SwaggerProvider<Schema>
+
 let o = OperandClient.Operands(Left = Some 4 ,Right = Some 4 )
-OperandClient(Host = "http://localhost:8080").PostAdd(o).Result
+let result = OperandClient(Host = "http://localhost:8080").PostAdd(o).Result
+
+printfn "result is %A" result
